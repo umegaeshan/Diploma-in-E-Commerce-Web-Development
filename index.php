b5bf88935d249feecf7ceccd737f9589961f7d91
Umega Eshan <umegaeshan@gmail.com>
2025-12-08 20:41:09 +0530

    add some styles and margins

--- index.php
+++ index.php
@@ -20,32 +20,39 @@
 
 	<!--  CREATE NAV BAR -->
 
-	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<nav class="navbar navbar-expand-lg bg-info-subtle">
 		<div class="container-fluid">
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<a class="navbar-brand" href="#"><i class="fa-brands fa-react fa-rotate-90 fa-2xl" style="color: #000000;"></i></a>
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+				<ul class="navbar-nav me-auto mb-3 mb-lg-2 fs-4 fw-bold">
 					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="/index">Home</a>
+						<a class="nav-link active ms-5" aria-current="page" href="/index">Home</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="/product">Product</a>
+						<a class="nav-link ms-5 me-4" href="/product">Product</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="/about">Product</a>
+						<a class="nav-link ms-5 me-4" href="/about">About</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="/contact">Product</a>
+						<a class="nav-link ms-5 me-4" href="/contact">Contact Us</a>
 					</li>
 
 				</ul>
-				<form class="d-flex" role="search">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-					<button class="btn btn-outline-success" type="submit">Search</button>
+				<form class="d-flex me-5" role="search">
+
+					<input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
+					<button class="btn btn-outline-success " type="submit">Search</button>
+
 				</form>
+				<div>
+					<button type="button" class="btn btn-success ms-5">Log In</button>
+					<button type="button" class="btn btn-outline-success">Register</button>
+				</div>
 			</div>
 		</div>
 	</nav>
